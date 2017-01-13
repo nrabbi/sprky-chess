9eed30e206426510f8efa1ada3846bae262f49b0
Ronald Gayda <ronald.gayda.13@gmail.com>
2017-01-13 16:49:07 +0000

    Generated Game and Move models

---
+++ app/models/game.rb
@@ -0,0 +1,17 @@
+class Game < ApplicationRecord
+  CREATED = 'created'
+  STARTED = 'started'
+  PLAYER_1_WON = 'player 1 won'
+  PLAYER_2_WON = 'player 2 won'
+  DRAW = 'draw'
+
+  enum status: {
+    created: CREATED,
+    started: STARTED,
+    player_1_won: PLAYER_1_WON,
+    player_2_won: PLAYER_2_WON,
+    draw: DRAW
+  }
+
+  has_many :moves
+end
