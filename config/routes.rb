d23981527abe735e73e69bab60e6964ce1e7d018
Sijia Davis <arwen@ksu.edu>
2017-01-14 23:56:03 +0000

    Create game controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'static_pages/home'
 
   root 'static_pages#home'
-
+  resources :games, only: [:new, :show, :create]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
