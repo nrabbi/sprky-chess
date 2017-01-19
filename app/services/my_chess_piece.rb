323ed1b9fd844bea94e2693b167127aecbf843d2
Ronald Gayda <ronald.gayda.13@gmail.com>
2017-01-19 18:38:38 +0000

    Added abstract method definition

--- app/services/my_chess_piece.rb
+++ app/services/my_chess_piece.rb
@@ -8,6 +8,14 @@ class MyChessPiece
     @position = Position.new(x, y)
   end
 
+  # Abstract Method(pieces : MyChessPiece[], destination: Position)
+  # -------------------
+  # Determines if another piece is between "self" and destination Position.
+  # The pieces array will hold all 32 pieces, including self.
+  def is_obstructed?(pieces, destination)
+    fail NotImplementedError, "Must be able to detect obstruction!"
+  end
+
   private
   def inside_board_boundaries?(x, y)
     x <= BOARD_END && x >= BOARD_START && y <= BOARD_END && y >= BOARD_START
