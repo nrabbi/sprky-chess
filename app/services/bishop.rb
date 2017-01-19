41decd5669909fb5c2260741f2886baa97febc00
Ronald Gayda <ronald.gayda.13@gmail.com>
2017-01-19 18:07:33 +0000

    Moved is_board_boundaries and at_destination to base class

--- app/services/bishop.rb
+++ app/services/bishop.rb
@@ -17,7 +17,6 @@ class Bishop < MyChessPiece
     curr_x = self.position.x
     curr_y = self.position.y
     while true do
-      puts curr_x.to_s + ',' +  curr_y.to_s
       if destination_is_to_right
         curr_x += 1
       else
@@ -30,23 +29,17 @@ class Bishop < MyChessPiece
         curr_y -= 1
       end
 
+      # check
       if inside_board_boundaries?(curr_x, curr_y) && !at_destination?(curr_x, curr_y, destination)
-        puts 'CHECKING SQUARE'
         if square_occupied?(pieces, Position.new(curr_x, curr_y))
-          puts 'SQUARE OCCUPIED'
           return true
         end
       else
-        if !inside_board_boundaries?(curr_x, curr_y)
-          puts 'OUTSIDE BOUNDARIES'
-        else
-          puts 'AT DESTINATION(' + destination.x.to_s + ',' + destination.y.to_s + ')'
-        end
         # made it this far without detecting obstruction, so no obstruction
         return false
       end
     end
-    puts 'MADE IT TO END'
+
     false
   end
 
@@ -54,9 +47,7 @@ class Bishop < MyChessPiece
 
   def square_occupied?(pieces, position)
     pieces.each do |piece|
-      puts piece.position.x.to_s + ' vs ' + position.x.to_s
       if piece.position.x == position.x && piece.position.y == position.y
-        puts 'OCCUPIED!'
         return true
       end
     end
@@ -64,11 +55,8 @@ class Bishop < MyChessPiece
     false
   end
 
-  def inside_board_boundaries?(x, y)
-    x < 8 && x > -1 && y < 8 && x > -1
-  end
-
   def at_destination?(x, y, destination)
     x == destination.x && y == destination.y
   end
+
 end
--- app/services/my_chess_piece.rb
+++ app/services/my_chess_piece.rb
@@ -1,8 +1,15 @@
 class MyChessPiece
   attr_accessor :type, :position
+  BOARD_START = 0
+  BOARD_END = 7
 
   def initialize(x, y)
     @type = 1
     @position = Position.new(x, y)
+  end
+
+  private
+  def inside_board_boundaries?(x, y)
+    x <= BOARD_END && x >= BOARD_START && y <= BOARD_END && y >= BOARD_START
   end
 end
