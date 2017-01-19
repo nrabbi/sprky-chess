fa57c630b4414454d6a5a1a2fb6f24daa17037fb
Ronald Gayda <ronald.gayda.13@gmail.com>
2017-01-19 07:24:44 +0000

    Created tests

--- spec/services/king_spec.rb
+++ spec/services/king_spec.rb
@@ -1,9 +1,45 @@
 require 'rspec'
+require 'rails_helper'
 
-describe 'My behaviour' do
+RSpec.describe "King" do
+  describe 'king#is_obstructed' do   # Assuming move is valid
 
-  it 'should do something' do
+    it 'should determine that a piece is between a king and a square' do
 
-    true.should == false
+      # 0,0,0,0,0,0,0,0
+      # 0,0,0,0,0,0,0,0
+      # 0,0,0,0,0,0,0,0
+      # 0,0,D,0,0,0,0,0 --> Destination is occupied
+      # 0,0,0,K,0,0,0,0
+      # 0,0,0,0,0,0,0,0
+      # 0,0,0,0,0,0,0,0
+      # 0,0,0,0,0,0,0,0
+
+      king = King.new(3, 3)
+      obstructor_piece = MyChessPiece.new(2, 4)
+      pieces = [king, obstructor_piece]
+      destination = Position.new(2, 4)
+      king.is_obstructed?(pieces, destination).should == true
+
+    end
+
+    it 'should determine that there is nothing between a king and a square' do
+
+      # 0,0,0,0,0,0,0,0
+      # 0,0,0,0,0,0,0,0
+      # 0,0,0,0,0,0,0,0
+      # 0,0,0,0,0,0,0,0
+      # 0,0,0,K,x,0,0,0 --> Destination is occupied
+      # 0,0,0,x,D,0,0,0
+      # 0,0,0,0,0,0,0,0
+      # 0,0,0,0,0,0,0,0
+
+      # No obstruction
+      king = King.new(3, 3)
+      destination = Position.new(4, 2)
+      pieces = [king, MyChessPiece.new(3, 2), MyChessPiece.new(4, 3)]
+
+      king.is_obstructed?(pieces, destination).should == false
+    end
   end
 end
