81c3ed7c207d08c4bccc3a862815fc8c600af002
Jesse Farber <jessefarber@earthlink.net>
2017-01-24 10:00:59 +0100

    Rewrote putting pieces on board, Stage 1

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,6 @@
 class GamesController < ApplicationController
+
+  helper_method :get_piece_at
   
   def new
   end
@@ -13,8 +15,24 @@ class GamesController < ApplicationController
   end
 
   def board
-    @starting_positions = StartingPositions::STARTING_POSITIONS
+    # @starting_positions = StartingPositions::STARTING_POSITIONS
     # @current_piece_positions = PieceMover::move_piece
+  end
+
+  # x in [0, 7]
+  # y in [0, 7]
+  def get_piece_at(x, y)
+    # returns nil if no piece @ position
+    starting_positions = StartingPositions::STARTING_POSITIONS
+
+    starting_positions.each do |piece| 
+      if piece.position.x == x && piece.position.y == y
+        return piece
+      end
+    end
+
+    nil
+
   end
 
 end
