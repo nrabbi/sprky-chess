8931cbd6801f8a22e90ca80b09df77ee431020ac
Nazmul Rabbi <nrabbi@outlook.com>
2017-01-28 22:33:06 +0000

    added pawns is_obstructed method and tests

--- app/services/pawn.rb
+++ app/services/pawn.rb
@@ -1,7 +1,24 @@
 class Pawn < ChessPiece
 
   def is_obstructed?(pieces, destination)
-    # IMPLEMENT HERE
+    return true unless inside_board_boundaries?(destination.x, destination.y)
+    destination.y > self.position.y ? (activePlayer = 1) : (activePlayer = 2)
+    distanceCheck = (destination.y - self.position.y).abs
 
+    if self.position.y == 1 && activePlayer == 1 or self.position.y == 6 && activePlayer == 2
+      return true if distanceCheck > 2
+      pawnAtStartingPosition = true
+    else
+      return true if distanceCheck > 1
+      pawnAtStartingPosition = false
+    end
+
+    pieces.each do |piece|
+      return true if piece.position.y == destination.y && piece.position.x == destination.x
+      return true if piece.position.y == (destination.y - 1) && piece.position.x == destination.x && activePlayer == 1 && pawnAtStartingPosition == true
+      return true if piece.position.y == (destination.y + 1) && piece.position.x == destination.x && activePlayer == 2 && pawnAtStartingPosition == true
+    end
+
+    return false
   end
 end
