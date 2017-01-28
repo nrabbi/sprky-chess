4a518e318fd6d38c87fb0eb9c1dc6f3f406afb32
Enrico Zschemisch <enrico@vicampus.com>
2017-01-28 08:41:32 +0100

    - fixed Bishop tests

--- spec/services/bishop_spec.rb
+++ spec/services/bishop_spec.rb
@@ -16,14 +16,14 @@ RSpec.describe "Bishop" do
       # 0,0,B,0,0,0,0,0
 
       # 1 obstruction
-      bishop = Bishop.new(2, 0)
-      obstructor_piece = ChessPiece.new(5, 3)
+      bishop = Bishop.new(:white, Position.new(2,0))
+      obstructor_piece = Pawn.new(:white, Position.new(5, 3))
       pieces = [bishop, obstructor_piece]
       destination = Position.new(7,5)
       expect(bishop.is_obstructed?(pieces, destination)).to eq true
 
       # 2 obstructions
-      obstructor_piece_2 = ChessPiece.new(3, 1)
+      obstructor_piece_2 = Pawn.new(:white, Position.new(3, 1))
       pieces << obstructor_piece_2
       expect(bishop.is_obstructed?(pieces, destination)).to eq true
     end
@@ -40,9 +40,9 @@ RSpec.describe "Bishop" do
       # 0,0,0,0,0,B,0,0
 
       # No obstruction
-      bishop = Bishop.new(5, 0)
-      pieces = [bishop, ChessPiece.new(1, 3), ChessPiece.new(1, 4), ChessPiece.new(2, 2), ChessPiece.new(2, 4),
-                ChessPiece.new(3, 1), ChessPiece.new(3, 3), ChessPiece.new(4, 2), ChessPiece.new(5, 1)]
+      bishop = Bishop.new(:white, Position.new(5, 0))
+      pieces = [bishop, Pawn.new(:white, Position.new(1, 3)), Pawn.new(:white, Position.new(1, 4)), Pawn.new(:white, Position.new(2, 2)), Pawn.new(:white, Position.new(2, 4)),
+                Pawn.new(:white, Position.new(3, 1)), Pawn.new(:white, Position.new(3, 3)), Pawn.new(:white, Position.new(4, 2)), Pawn.new(:white, Position.new(5, 1))]
       destination = Position.new(2, 3)
 
       expect(bishop.is_obstructed?(pieces, destination)).to eq false
