00ac641dc0a35c389d4fb997425c78e76bab8e14
Jesse Farber <jessefarber@earthlink.net>
2017-01-31 15:43:36 +0100

    Install simple_form gem, begin move#new and tests

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -1,4 +1,7 @@
 class MovesController < ApplicationController
   def index
   end
+
+  def new
+  end
 end
--- spec/controllers/moves_controller_spec.rb
+++ spec/controllers/moves_controller_spec.rb
@@ -8,4 +8,11 @@ RSpec.describe MovesController, type: :controller do
     end
   end
 
+  describe "moves#new action" do
+    it "should successfully show the new form" do
+      get :new, game_id: 2
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
