6e1ce7af34c30bbffc19360f860dfbe7b90e0cd4
Ronald Gayda <ronald.gayda.13@gmail.com>
2017-02-01 19:25:50 +0000

    Made table drop operations safe

--- db/migrate/20170201161340_drop_chess_piece_table.rb
+++ db/migrate/20170201161340_drop_chess_piece_table.rb
@@ -1,5 +1,5 @@
 class DropChessPieceTable < ActiveRecord::Migration[5.0]
   def change
-        drop_table :chess_pieces
+    drop_table :chess_pieces if ActiveRecord::Base.connection.table_exists? :chess_pieces
   end
 end
