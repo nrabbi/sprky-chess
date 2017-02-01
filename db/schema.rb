6de50e7b0e7caf2329f0895ec7cb8d188f94d147
Jesse Farber <jessefarber@earthlink.net>
2017-02-01 15:43:25 +0100

    Basic CRUD for Game complete, with routes/actions/views.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170201044420) do
+ActiveRecord::Schema.define(version: 20170201140532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chess_pieces", force: :cascade do |t|
+    t.string   "type"
+    t.integer  "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "games", force: :cascade do |t|
     t.integer  "player_1_id"
@@ -21,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170201044420) do
     t.string   "status"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "name"
     t.index ["player_1_id"], name: "index_games_on_player_1_id", using: :btree
     t.index ["player_2_id"], name: "index_games_on_player_2_id", using: :btree
     t.index ["status"], name: "index_games_on_status", using: :btree
@@ -52,6 +60,23 @@ ActiveRecord::Schema.define(version: 20170201044420) do
     t.string   "uid"
     t.index ["email"], name: "index_players_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_players_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
