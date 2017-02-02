d474f4303c8e2707b7315f9ce95eec96a25c9545
Jesse Farber <jessefarber@earthlink.net>
2017-02-02 12:06:51 +0100

    Move create action and form. (Also corrected board square colors.)

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -3,5 +3,18 @@ class MovesController < ApplicationController
   end
 
   def new
+    @game = Game.find(params[:game_id])
+    @move = Move.new
+  end
+
+  def create
+    @game = Game.find(params[:game_id])
+    @move = @game.moves.create(move_params)
+    redirect_to game_path(@game)
+  end
+
+  private
+  def move_params
+    params.require(:move).permit(:from, :to)
   end
 end
