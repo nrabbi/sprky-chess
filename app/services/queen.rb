e2beb06d3dff9cf07effd8dab20f351fe4c70dd8
Jesse Farber <jessefarber@earthlink.net>
2017-02-06 13:15:10 +0100

    Add queen.is_valid? method and tests

--- app/services/queen.rb
+++ app/services/queen.rb
@@ -5,6 +5,19 @@ class Queen < ChessPiece
 
   end
 
+  def is_valid?(destination)
+    x_diff = (self.position.x - destination.x).abs
+    y_diff = (self.position.y - destination.y).abs
+    valid_diagonal_move = (x_diff == y_diff)
+
+    x_only_move = (self.position.x - destination.x).abs > 0 && (self.position.y - destination.y).abs == 0
+    y_only_move = (self.position.y - destination.y).abs > 0 && (self.position.x - destination.x).abs == 0
+
+    moved = !self.position.equals?(destination)
+
+    (valid_diagonal_move || x_only_move || y_only_move) && moved && inside_board_boundaries?(destination.x, destination.y)
+  end
+
   def html_icon
     @color == :white ? "&#9813;" : "&#9819;"
   end
