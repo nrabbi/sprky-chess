db179427203e5780b57cd46394c6f96acfff1b29
Enrico Zschemisch <enrico@vicampus.com>
2017-02-08 03:06:29 -0500

    improved Move's display on the game board

--- app/models/move.rb
+++ app/models/move.rb
@@ -3,13 +3,4 @@ class Move < ApplicationRecord
   validates :from, presence: true
   validates :to, presence: true
 
-  # OLD STUFF
-  # attr_accessor :to
-  # attr_reader :from
-
-  # def initialize(x, y)
-  #   @from = Position.new(x, y)
-  #   @to = Position.new(99, 99)
-  # end
-
 end
