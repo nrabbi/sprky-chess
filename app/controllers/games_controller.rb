c1275521317d3ee3c70d4c275393be76996a9fa1
Jesse Farber <jessefarber@earthlink.net>
2017-02-09 11:41:25 +0100

    Game status set to 'created' after game#create

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,6 +10,7 @@ class GamesController < ApplicationController
   def create
     @game = Game.create(game_params)
     if @game.valid?
+      @game.created!
       redirect_to game_path(@game)
     else
       render :new, status: :unprocessable_entity
