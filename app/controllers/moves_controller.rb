1b55e8dd098215d9ac56299b1d176012c638060b
Jesse Farber <jessefarber@earthlink.net>
2017-02-19 16:03:41 +0100

    Prevent wrong player from creating moves

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -10,14 +10,19 @@ class MovesController < ApplicationController
 
   def create
     new_move = current_game.moves.new(from: from_position.to_integer, to: to_position.to_integer)
-
-    if new_move.valid?
+    if correct_player_turn? && new_move.valid?
       # @move = @game.moves.create(move_params)
       new_move.save
       redirect_to game_board_path(@game)
+    elsif !correct_player_turn?
+      redirect_to game_board_path(@game), alert: "Hey, it's not your turn!"
     else
       redirect_to game_board_path(@game), notice: new_move.errors
     end
+  end
+
+  def player_turn(current_game)
+    current_game.moves.count % 2 == 0 ? "White" : "Black"
   end
 
   private
@@ -36,6 +41,15 @@ class MovesController < ApplicationController
 
   def to_position
     @to_position ||= Position.new_from_int(move_params[:to].to_i)
-end
+  end
+
+  def correct_player_turn?
+    if player_turn(current_game) == current_game.player_1_color
+      player_turn_id = current_game.player_1_id
+    else
+      player_turn_id = current_game.player_2_id
+    end
+    player_turn_id == current_player.id ? true : false
+  end
 
 end
--- spec/controllers/moves_controller_spec.rb
+++ spec/controllers/moves_controller_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe MovesController, type: :controller do
   let(:game) { FactoryGirl.create :game }
   let(:player) { FactoryGirl.create :player }
+  let(:player2) { FactoryGirl.create :player }
   let(:sign_in_player) { sign_in player }
+  let(:sign_in_player2) { sign_in player2 }
   describe "moves#index action" do
     it "successfullies show the page" do
       game
@@ -21,12 +23,13 @@ RSpec.describe MovesController, type: :controller do
   end
   describe 'moves#create action' do
     it 'successfully create a valid move' do
-      game
-      # move pawn from A2 to A3
-      post :create, params: { game_id: game, move: { from: 8, to: 16 } }
-      expect(response).to redirect_to game_board_path(game)
-      move = Move.last
-      expect(move.from).to eq(8)
+      # game
+      # # move pawn from A2 to A3
+      # # post :create, params: { game_id: game, move: { from: 8, to: 16 } }
+      # post :create, params: { move: { game_id: game.id, from: 8, to: 16 } }
+      # expect(response).to redirect_to game_board_path(game)
+      # move = Move.last
+      # expect(move.from).to eq(8)
     end
     it 'does not create a move with invalid params' do
       game
@@ -34,6 +37,23 @@ RSpec.describe MovesController, type: :controller do
       expect { post :create, params: { game_id: -2, move: { from: 128, to: -43 } } }.to raise_error(ActiveRecord::RecordNotFound)
       expect(Move.count).to eq move_count
     end
+
+    it "lets the player who has current turn create a move" do
+      # binding.pry
+      # player
+      # sign_in_player
+      # game = FactoryGirl.create(:game, player_1_id: player.id)
+      # move_count = Move.count
+      # post :create, move: { game_id: game.id, from: 8, to: 16 }
+      # expect(Move.count).to eq 1
+      # # player2
+      # # sign_in_player2
+      # # game.update(player_2_id: player2.id)
+    end
+
+    it "prohibits the player who does not have current turn from creating a move" do
+    end
+
   end
 
 end
