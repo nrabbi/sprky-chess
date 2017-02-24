9b06303efbf7ea84546f1e37563fc0b4c003815f
Jesse Farber <jessefarber@earthlink.net>
2017-02-24 21:10:39 +0100

    Initial actioncable channel setup

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,19 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_player
+
+    def connect
+      self.current_player = find_verified_player
+      logger.add_tags 'ActionCable', current_player.email
+    end
+    private
+    def find_verified_player
+      verified_player = Player.find_by(id: cookies.signed['player.id'])
+      if verified_player && cookies.signed['player.expires_at'] > Time.now
+        verified_player
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
