5d89f75519831612158f0f5b239156c5f8aece56
Ronald Gayda <ronald.gayda.13@gmail.com>
2017-03-30 02:11:36 +0000

    Added tests

--- spec/controllers/moves_controller_spec.rb
+++ spec/controllers/moves_controller_spec.rb
@@ -83,6 +83,32 @@ RSpec.describe MovesController, type: :controller do
       expect(Move.count).to eq 0
     end
 
+
+    it "prohibits a new move if game is over" do
+      player
+      player2
+      game = FactoryGirl.create(:game, player_1_id: player.id, player_2_id: player2.id, player_2_color: "Black", status: Game::STARTED)
+      post :create, params: { game_id: game.id, move: { from: 8, to: 16 } }
+      last_move = Move.last
+      game.player_1_won!
+      post :create, params: { game_id: game.id, move: { from: 16, to: 24 } }
+      attempted_move = Move.last
+      expect(attempted_move.id).to eq(last_move.id)
+    end
+
+    it "sets the game status to one of {player_1_won, player_2_won} after checkmate" do
+      player
+      player2
+      game = FactoryGirl.create(:game, player_1_id: player.id, player_2_id: player2.id, player_2_color: "Black", status: Game::STARTED)
+      # Fool's Mate
+      post :create, params: { game_id: game.id, move: { from: 13, to: 21 } }
+      post :create, params: { game_id: game.id, move: { from: 52, to: 36 } }
+      post :create, params: { game_id: game.id, move: { from: 14, to: 30 } }
+      post :create, params: { game_id: game.id, move: { from: 59, to: 31 } }
+
+      expect(game.status).to eq(Game:: PLAYER_2_WON)
+    end
+
     context "when Castler get valid castle" do
       it "returns updated move" do
         player
